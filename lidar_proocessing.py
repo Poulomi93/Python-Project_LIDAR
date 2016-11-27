cbafe9698bcdd9be5450280b24d26e292f85c0ab
Kunal6475 <kunalsingharoy@gmail.com>
2016-11-26 19:07:18 -0500

    Updated the functionality of finding road data from lidar data points using shape file.

--- lidar_proocessing.py
+++ lidar_proocessing.py
@@ -1,24 +1,29 @@
+"""Module for processing of lidar files"""
 from liblas import file
 from PIL import Image
+import pyproj
 
 
-def getMaxMinCoordinates(lidarfile):
-    max_x = -1
-    min_x = 10000000000000
-    max_y = -1
-    min_y = 1000000000000
-    for p in lidarfile:
-        if p.x > max_x:
-            max_x = int(p.x)
-        if p.x < min_x:
-            min_x = int(p.x)
-        if p.y > max_y:
-            max_y = int(p.y)
-        if p.y < min_y:
-            min_y = int(p.y)
-    return min_x, max_x, min_y, max_y
+
+def convert_northFL_to_wgs84(easting, northing):
+    '''
+
+    Parameters:
+        easting: in feet
+        northing: in feet
+    '''
+    #This are constants that are required for transformation
+    wgs84 = pyproj.Proj(init='epsg:4326')
+    northFL = pyproj.Proj(init='esri:102660')
+    return pyproj.transform(northFL, wgs84,
+                            easting * 0.3048, northing * 0.3048)
+
 
 def getMaxMinCoordinatesforlidarfile(lidarfile):
+    '''This is a helper function that gets the max and min co-ordinates of the data points present in lidar file. This is an additional function
+      as the max min co-ordinates can be easily known from header information present in the lidar file
+     '''
+    lidarfile.seek(0)
     max_x = -1
     min_x = 10000000000000
     max_y = -1
@@ -37,6 +42,7 @@ def getMaxMinCoordinatesforlidarfile(lidarfile):
 
 
 def createImageFile(lidarfile, filename,image_size_x,image_size_y, maxmincoordinates):
+    '''This function creates the image of the specified size'''
     i = Image.new(mode='RGB', size=(image_size_x,image_size_y), color=None)
     for p in f:
         i.putpixel((int(p.x) - maxmincoordinates[0], int(p.y) - maxmincoordinates[2]),
@@ -44,119 +50,62 @@ def createImageFile(lidarfile, filename,image_size_x,image_size_y, maxmincoordin
     i.save(filename, format="JPEG")
 
 def getlistoflidardata(lidarfile):
+    '''This function returns the list of lidar data points'''
     listOfLidardata=[]
     for p in lidarfile:
         listOfLidardata.append(p)
     return listOfLidardata
 
 def getlidardatafile(lidar_fileName):
+    '''This function returns the lidar file after opening it'''
     f = file.File(lidar_fileName, mode='r')
     return f
 
-def createImageforregion(lidarfile,maxmincoordinates,imagefile):
+def createImageforregion(lidarfile, maxmincoordinates, imagefile):
+    '''This function creates the image for a particular region of the lidar file'''
     status = input('"Do you waant to create image for a particular region of lidar file? Enter Y/Yes or N/No')
-    status=status.upper()
-    if status =='Y' or status =='YES':
-        print("min x coordinate:",maxmincoordinates[0],"max x coordinate",maxmincoordinates[1],"min y coordinate",maxmincoordinates[2],"max y coordinate",maxmincoordinates[3])
-        upper_x=  int(input("Give the x coordinate of the uppermost left point in the diagonal"))
-        upper_y=int(input("Give the y coordinate of the uppermost top point in the diagonal"))
-        lower_x= int(input('Give the x coordinate of the lowermost right point in the diagonal'))
-        lower_y= int(input("Give the y coordinate of the lowermost right point in the diagonal"))
+    status = status.upper()
+    if status == 'Y' or status == 'YES':
+        print("min x coordinate:", maxmincoordinates[0], "max x coordinate", maxmincoordinates[1], "min y coordinate", maxmincoordinates[2], "max y coordinate", maxmincoordinates[3])
+        upper_x = int(input("Give the x coordinate of the uppermost left point in the diagonal"))
+        upper_y = int(input("Give the y coordinate of the uppermost top point in the diagonal"))
+        lower_x = int(input('Give the x coordinate of the lowermost right point in the diagonal'))
+        lower_y = int(input("Give the y coordinate of the lowermost right point in the diagonal"))
         i = Image.new(mode='RGB',size=(lower_x-upper_x+1,lower_y-upper_y+1),color=None)
         for p in lidarfile:
-            if int(p.x)>=upper_x and int(p.x)<=lower_x and int(p.y)>=upper_y and int(p.y)<=lower_y:
+            if int(p.x) >= upper_x and int(p.x) <=lower_x and int(p.y)>=upper_y and int(p.y)<=lower_y:
                 i.putpixel((int(p.x)-upper_x,int(p.y)-upper_y),(p.color.red,p.color.green,p.color.blue))
         i.save(imagefile,format="JPEG")
     else:
         print("Exiting the application")
 
 def printHeaderInformation(lidarfile):
+    ''' This function prints the header information of the lidar file'''
     header = lidarfile.header
-    print("Major Version:",header.major_version,", Minor Version:",header.minor_version)
-    print("Data format id:",header.data_format_id)
-    print("Header offset:",header.offset)
-    print("Header max:",header.max)
-    print("Header min:",header.min)
-    print("sr.proj4",header.srs.proj4)
-    print("srs.proj4.getproj4",header.srs.get_proj4())
+    print("Major Version:", header.major_version,", Minor Version:", header.minor_version)
+    print("Data format id:", header.data_format_id)
+    print("Header offset:", header.offset)
+    print("Header max:", header.max)
+    print("Header min:", header.min)
+    print("sr.proj4", header.srs.proj4)
+    print("srs.proj4.getproj4", header.srs.get_proj4())
+    print("Header Max latitude longitude:", convert_northFL_to_wgs84(header.max[0],header.max[1]))
+    print("Header Min latitude longitude:", convert_northFL_to_wgs84(header.min[0], header.min[1]))
 
+def getHeaderInformation(lidarfile):
+    '''This function gets the header of the lidar data file'''
+    header = lidarfile.header
+    return header
 
-
-if __name__=="__main__" :
+if __name__== "__main__" :
     lidar_fileName = "las_tile_46138/2035000.25_541249.75_2036250.25_539999.75.las"
-    #lidar_fileName = 'LAS12_Sample_withRGB_Quick_Terrain_Modeler_fixed.las.part'
-    #lidar_fileName = '/home/kunal/Downloads/LAS12_Sample_withRGB_Quick_Terrain_Modeler_fixed.las.part'
     image_fileName = "lidar_image"
-    #image_fileName = "/home/kunal/lidar_image"
-    image_fileregion='lidar_image_rectangle'
-    #image_fileregion='/home/kunal/lidar_image_rectangle'
+    image_fileregion = 'lidar_image_rectangle'
     f = file.File(lidar_fileName, mode='r')
     printHeaderInformation(f)
-    maxmincoordinates = getMaxMinCoordinates(f)
-    f.seek(0)
+    maxmincoordinates = getMaxMinCoordinatesforlidarfile(f)
     image_size_x = maxmincoordinates[1]-maxmincoordinates[0]+1
-    image_size_y= maxmincoordinates[3]-maxmincoordinates[2]+1
-    createImageFile(f, image_fileName,image_size_x,image_size_y, maxmincoordinates)
+    image_size_y = maxmincoordinates[3]-maxmincoordinates[2]+1
+    createImageFile(f, lidar_fileName.replace('/','')[:-4] + 'lidarimage', image_size_x, image_size_y, maxmincoordinates)
     f.seek(0)
-    createImageforregion(f,maxmincoordinates,image_fileregion)
-
-
-
-
-
-
-
-
-
-
-# header = f.header
-# print(header.major_version,header.minor_version)
-# print(header.data_format_id)
-# print(header.offset)
-# print(header.point_records_count)
-# print(header.max)
-"""
-max_x=-1
-min_x=10000000000000
-max_y = -1
-min_y=1000000000000
-
-for p in f:
-    if p.x>max_x:
-        max_x = int(p.x)
-    if p.x<min_x:
-        min_x=int(p.x)
-    if p.y>max_y:
-        max_y=int(p.y)
-    if p.y<min_y:
-        min_y=int(p.y)
-
-print("Min x co-ordinate",min_x)
-print("Max x co-ordinate",max_x)
-print("Min y co-ordinate",min_y)
-print("Max y co-ordinate",max_y)
-
-#p= f.read(0)
-#print(p.x,p.y,p.z,p.color.red,p.color.green,p.color.blue)
-count = 0
-i=Image.new(mode='RGB',size=(1000,1000),color=None)
-index =0
-
-for p in f:
-   #print('X={0} Y={1} Z={2} Red={3} Green={4} Blue={5}'.format(p.x,p.y,p.z,p.color.red,p.color.green,p.color.blue))
-   i.putpixel((count,index),(p.color.red,p.color.green,p.color.blue))
-   #count=count+1
-   index = index + 1
-   if index == 100:
-       index = 0
-       count = count +1
-   if count>99:
-       break
-   #c = p.color
-   #print(c.red)
-   #print(c.blue)
-   #print(c.green)
-   #count =count +1
-i.save('/home/kunal/lidar_image',format="JPEG")
-#print(count)
-"""
+    createImageforregion(f, maxmincoordinates, image_fileregion)
--- shapefile_processing.py
+++ shapefile_processing.py
@@ -5,12 +5,17 @@ import lidar_proocessing
 import pyproj
 from PIL import Image
 
+#Constants that are importnat for transformation
 wgs84 = pyproj.Proj(init='epsg:4326')
 northFL = pyproj.Proj(init='esri:102660')
 no_matched = 0
 
 def convert_wgs84_to_northFL(lon, lat):
     '''
+
+    Parameters:
+      lon:Longitude of a place
+      lat:Latitude of a place
     '''
     x, y = pyproj.transform(wgs84, northFL, lon, lat)
     return x / 0.3048, y / 0.3048
@@ -26,69 +31,74 @@ def convert_northFL_to_wgs84(easting, northing):
                             easting * 0.3048, northing * 0.3048)
 
 def createImageFile(image_size_x,image_size_y):
+    '''Creates the image file of particular size'''
     image_f = Image.new(mode='RGB', size=(image_size_x, image_size_y), color=None)
     return image_f
+
+def getlistoflatlong(listoflidardatapoints):
+    """This function will return the list of latitude longitude corresponding to all the data points in the lidar file"""
+    latlonglist= []
+    for i in range(0, len(listoflidardata)):
+        latlonglist.append(convert_northFL_to_wgs84(listoflidardatapoints[i].x, listoflidardatapoints[i].y))
+    return latlonglist
+
+def getcroppedlistofshapes(shapefile,latlongmax,latlongmin):
+    '''This function returns the cropped list of latitude and longitude'''
+    croppedlistofshapes =[]
+    for shp in sf.iterShapes():
+        #print(shp.shapeType)
+        if (shp.bbox[0]<=latlongmax[0] and shp.bbox[0]>=latlongmin[0]):
+            if (shp.bbox[1]<=latlongmax[1] and shp.bbox[1]>=latlongmin[1]):
+                print("Added to the shapefile list",shp.bbox[0],shp.bbox[1])
+                croppedlistofshapes.append(shp)
+        if (shp.bbox[2]<=latlongmax[0] and shp.bbox[2]>=latlongmin[0]):
+            if (shp.bbox[3]<=latlongmax[1] and shp.bbox[3]>=latlongmin[1]):
+                print("Added to the shapefile list",shp.bbox[2],shp.bbox[3])
+                croppedlistofshapes.append(shp)
+    return croppedlistofshapes
+
+def getsetofmatchedlidarpoints(listofcroppedshapes,listoflatlong,listoflidardata):
+    '''This function will return a set of datapoints that matches the cropped shapes points'''
+    setofmatchedpoints = set()
+    no_matched = 0
+    for shapes in croppedshapes:
+        for p in shapes.points:
+            for i in range(len(listoflatlong)):
+                if abs(p[0] - listoflatlong[i][0]) < 0.0004 and abs(p[1] - listoflatlong[i][1]) < 0.0004:
+                    setofmatchedpoints.add(listoflidardata[i])
+                    no_matched = no_matched + 1
+    print("no matched:", no_matched)
+    print('Length of matched points:', len(setofmatchedpoints))
+    return setofmatchedpoints
+
+def colormatcheddatapoints(setoflidaradata,imagefile,maxmincoordinates,imagefileName):
+    '''This function will color the matched data points'''
+    for p in setoflidaradata:
+        imagefile.putpixel((int(p.x) - maxmincoordinates[0], int(p.y) - maxmincoordinates[2]),
+                   (p.color.red, p.color.green, p.color.blue))
+    print('image file name:',imagefileName)
+    imagefile.save(imagefileName,format="jpeg")
 
 sf = shapefile.Reader('tl_2016_12073_roads/tl_2016_12073_roads.shp')
 lidar_fileName = "las_tile_46138/2035000.25_541249.75_2036250.25_539999.75.las"
 lidar_file = lidar_proocessing.getlidardatafile(lidar_fileName)
 listoflidardata= lidar_proocessing.getlistoflidardata(lidar_file)
+lidarfile_header = lidar_proocessing.getHeaderInformation(lidar_file)
 print("No of data points:",len(listoflidardata))
-maxmincoordinates = lidar_proocessing.getMaxMinCoordinates(lidar_file)
+maxmincoordinates = lidar_proocessing.getMaxMinCoordinatesforlidarfile(lidar_file)
 image_size_x = maxmincoordinates[1] - maxmincoordinates[0] + 1
 image_size_y = maxmincoordinates[3] - maxmincoordinates[2] + 1
 imagefile = createImageFile(image_size_x,image_size_y)
-#print(maxmincoordinates[0],',',maxmincoordinates[1],',',maxmincoordinates[2],',',maxmincoordinates[3])
-listoflatlong = []
-for i in range(0,len(listoflidardata)):
-    listoflatlong.append(convert_northFL_to_wgs84(listoflidardata[i].x, listoflidardata[i].y))
-#for p in listoflidardata:
-#    listoflatlong.append(convert_northFL_to_wgs84(p.x,p.y))
-
-#for p in listoflatlong:
-    #print(p)
-#    pass
-for shp in sf.iterShapes():
-    for i in range(0,len(listoflatlong)):
-        if (shp.bbox[0]<=listoflatlong[i][0] and listoflatlong[i][0]<=shp.bbox[2]):
-            if (shp.bbox[1]<=listoflatlong[i][1] and listoflatlong[i][1]<=shp.bbox[3]):
-                #print('matched')
-                imagefile.putpixel((int(listoflidardata[i].x) - maxmincoordinates[0], int(listoflidardata[i].y) - maxmincoordinates[2]),
-                           (listoflidardata[i].color.red, listoflidardata[i].color.green, listoflidardata[i].color.blue))
-                no_matched = no_matched+1
-
-#for shp in sf.iterShapes():
-#    for p in shp.points:
-#        for i in range(0,len(listoflatlong)):
-#           print(round(p[0],2),round(p[1],2),round(listoflatlong[i][0],2),round(listoflatlong[i][1],2))
-#           if round(p[0],2)==round(listoflatlong[i][0],2) and round(p[1],2)== round(listoflatlong[i][1],2):
-#               print('matched')
-#               imagefile.putpixel((int(listoflidardata[i].x) - maxmincoordinates[0], int(listoflidardata[i].y) - maxmincoordinates[2]),
-#                                  (listoflidardata[i].color.red, listoflidardata[i].color.green, listoflidardata[i].color.blue))
-#               no_matched = no_matched+1
-
-#for shp in sf.iterShapes():
-    #print("bbox:",shp.bbox)
-#    for p in listoflatlong:
-#        if (shp.bbox[0]<=p[0] and p[0]<=shp.bbox[2]):
-#            if (shp.bbox[1]<=p[1] and p[1]<=shp.bbox[3]):
-#                print('matched')
-#                no_matched = no_matched+1
-imagefile.save("road_image", format="JPEG")
-print("No of data points matched",no_matched)
-
-#print(listoflidardata)
-#shapes = sf.shapes()
-#print(len(shapes))
-#for shp in sf.iterShapes():
-    #print("Shape Type: ",shp.shapeType, "bbox: ",shp.bbox,"parts: ",shp.parts ,"points: ", shp.points)
-#    for p in shp.points:
-        #print(p[0],p[1])
-#        x , y =convert_wgs84_to_northFL(p[1],p[0])
-        #print('Trying to check x,:',int(x),'y:',int(y))
-#        for lidarpoint in listoflidardata:
-#            if int(lidarpoint.x) == int(x)  and int(lidarpoint.y) == int(y):
-#                print('Matched')
-#                no_matched = no_matched+1
-
-#print(no_matched)
+listoflatlong = getlistoflatlong(listoflidardata)
+maxlatlong = convert_northFL_to_wgs84(lidarfile_header.max[0],lidarfile_header.max[1])
+minlatlong = convert_northFL_to_wgs84(lidarfile_header.min[0],lidarfile_header.min[1])
+print(maxlatlong)
+print(minlatlong)
+print("Number of shape file records:",len(list(sf.iterShapes())))
+croppedshapes = getcroppedlistofshapes(sf,maxlatlong,minlatlong)
+print(len(croppedshapes))
+print(croppedshapes[0].bbox)
+print(croppedshapes[1].bbox)
+setofmatchedlidarpoints=getsetofmatchedlidarpoints(croppedshapes,listoflatlong,listoflidardata)
+colormatcheddatapoints(setofmatchedlidarpoints,imagefile,maxmincoordinates,lidar_fileName.replace('/','')[:-4]+'road_image')
+lidar_file.close()
